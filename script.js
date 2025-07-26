d1e9721567e90db136b6e95ef49d1d6b75450edd
Priscila <priscilaorejana@gmail.com>
2025-07-25 21:20:40 -0400

    Adiciona botão de modo escuro e botão de rolagem ao topo

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+function toggleDarkMode() {
+    const body = document.body;
+    const darkIcon = document.getElementById("dark-icon");
+    const lightIcon = document.getElementById("light-icon");
+
+    body.classList.toggle("darkmode");
+    
+    if (body.classList.contains("darkmode")) {
+        darkIcon.style.display = "none";
+        lightIcon.style.display = "block";
+    } else {
+        darkIcon.style.display = "block";
+        lightIcon.style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
